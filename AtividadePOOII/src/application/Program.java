18e40e3a1773e1e15628b170ec37faa2183f6c7a
Yan Rezende <yanrezendelucas04@gmail.com>
2024-08-19 23:34:17 -0300

    Atividade/Prova POOII

--- AtividadePOOII/src/application/Program.java
+++ AtividadePOOII/src/application/Program.java
@@ -2,58 +2,93 @@ package application;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.util.Scanner;
 
 import entities.Aluno;
 import jdbc.AlunoJDBC;
 
-
 public class Program {
 
 	public static void main(String[] args) {
-		
+
 		try {
-        	
-            int opcao = 0;
-            Scanner console = new Scanner(System.in);
-            
-            do {
-            	System.out.println("####### Menu #######"
-            						+ "\n1 - Cadastrar"
-            						+ "\n2 - Listar"
-            						+ "\n3 - Alterar"
-            						+ "\n4 - Excluir"
-            						+ "\n5 - Sair");
-            	System.out.println("\n\tOp��o:");
-            	opcao = Integer.parseInt(console.nextLine());
-            	
-            	if (opcao == 1) {
-            	
-            		Aluno a = new Aluno();
-            		AlunoJDBC acao = new AlunoJDBC();
-            		
-            		
-            		System.out.println("\n ### Cadastrar Aluno ### \n\r");
-            		
-            		System.out.print("Nome: ");
-            		a.setNome(console.nextLine());
-            		
-            		System.out.print("Sexo: ");
-            		a.setSexo(console.nextLine());
-            	
-            		System.out.print("Data de Nascimento (dd-mm-aaaa): ");
-            		DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-            		a.setDt_nasc( LocalDate.parse(console.nextLine(), formato) );
-            		
-            		acao.salvar(a);
-            		console.nextLine();
-            		System.out.println("\n\n\n\n");
-            	}
-            	
-            } while(opcao != 5);
-            
-        } catch (Exception e){
-            System.out.println("Erro: " + e);
-        }
-	} 
+
+			int opcao = 0;
+			Scanner console = new Scanner(System.in);
+			AlunoJDBC acao = new AlunoJDBC();
+			do {
+				System.out.println("\n####### Menu #######" + "\n1 - Cadastrar" + "\n2 - Listar" + "\n3 - Alterar"
+						+ "\n4 - Excluir" + "\n5 - Sair");
+				System.out.println("\nOpção:");
+				opcao = Integer.parseInt(console.nextLine());
+				switch (opcao) {
+				case 1: {
+					Aluno a = new Aluno();
+
+					System.out.println("\n ### Cadastrar Aluno ### \n\r");
+
+					System.out.print("Nome: ");
+					a.setNome(console.nextLine());
+
+					System.out.print("Sexo: ");
+					a.setSexo(console.nextLine());
+
+					System.out.print("Data de Nascimento (dd-mm-aaaa): ");
+					DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+					a.setDt_nasc(LocalDate.parse(console.nextLine(), formato));
+
+					acao.salvar(a);
+					System.out.println("\nCadastro do aluno realizado com sucesso!");
+					break;
+				}
+				case 2: {
+					List<Aluno> lista = acao.listar();
+					System.out.println("\nLISTAGEM DE ALUNOS");
+					for (Aluno aluno : lista) {
+						System.out
+								.println("**************************************************************************");
+						System.out.println("Nome: " + aluno.getNome() + ", Sexo: " + aluno.getSexo() + ", Data Nasc.: "
+								+ aluno.getDt_nasc() + ", Id: " + aluno.getId());
+						System.out
+								.println("**************************************************************************");
+					}
+					System.out.println("FIM DA LISTAGEM\n");
+					break;
+				}
+				case 3: {
+					System.out.print("\n Informe o ID de quem deseja alterar: ");
+					int idAlterar = console.nextInt();
+					
+					console.nextLine();
+
+					System.out.print("\n Insira o valor para nome: ");
+					String nomeAlterar = console.nextLine();
+
+					System.out.print("\n Insira o valor para o sexo: ");
+					String sexoAlterar = console.nextLine();
+
+					System.out.print("\n Informe o valor para a data de nascimento: ");
+					DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+					LocalDate dtNascimentoAlterar = LocalDate.parse(console.nextLine(), formato);
+
+					acao.alterar(idAlterar, nomeAlterar, sexoAlterar, dtNascimentoAlterar);
+					System.out.println("\nAluno alterado com sucesso!");
+					break;
+				}
+				case 4: {
+					System.out.print("\n Informe o ID de quem deseja excluir: ");
+					acao.apagar(console.nextInt());
+					break;
+				}
+				default:
+					break;
+				}
+			} while (opcao != 5);
+			System.out.println("\n\n\nPROGRAMA FINALIZADO!");
+			acao.fecharConexaoPst();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
--- AtividadePOOII/src/jdbc/AlunoJDBC.java
+++ AtividadePOOII/src/jdbc/AlunoJDBC.java
@@ -3,7 +3,10 @@ package jdbc;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 import entities.Aluno;
@@ -14,37 +17,95 @@ public class AlunoJDBC {
 	String sql;
 	PreparedStatement pst;
 	
+	public void fecharConexaoPst() {
+		try {
+			pst.close();
+		} catch (Exception e) {
+		}
+	}
+
 	public void salvar(Aluno a) throws SQLException {
-		
+
 		try {
 			con = db.getConexao();
 			sql = "INSERT INTO aluno (nome, sexo, dt_nasc) VALUES (?,?, ?)";
 			pst = con.prepareStatement(sql);
 			pst.setString(1, a.getNome());
 			pst.setString(2, a.getSexo());
-			Date dataSql = Date.valueOf( a.getDt_nasc() );
+			Date dataSql = Date.valueOf(a.getDt_nasc());
 			pst.setDate(3, dataSql);
 			pst.executeUpdate();
-			System.out.println("\nCadastro do aluno realizado com sucesso!");
-			
+
 		} catch (Exception e) {
 			System.out.println(e);
-		}
-		finally {
-			pst.close();
-			db.closeConexao();
+		} finally {
+			//pst.close();
 		}
 	}
-	
-	public List<Aluno> listar(){
-		return null;	
+
+	public List<Aluno> listar() throws Exception {
+		try {
+			List<Aluno> listaAluno = new ArrayList<Aluno>();
+			con = db.getConexao();
+			sql = "select * from aluno";
+			pst = con.prepareStatement(sql);
+			ResultSet rs = pst.executeQuery();
+
+			while (rs.next()) {
+				Aluno a = new Aluno();
+
+				a.setId(rs.getInt(1));
+				a.setNome(rs.getString(2));
+				a.setSexo(rs.getString(3));
+				a.setDt_nasc(rs.getDate(4).toLocalDate());
+				
+				listaAluno.add(a);
+			}
+			return listaAluno;
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			//pst.close();
+		}
+		return new ArrayList<Aluno>();
 	}
-	
-	public void apagar(int id) {
-		
+
+	public void apagar(int id) throws Exception {
+		try {
+			con = db.getConexao();
+			sql = "delete from aluno where id = ?";
+			pst = con.prepareStatement(sql);
+			pst.setInt(1, id);
+			pst.executeUpdate();
+			System.out.println("\nAluno excluido com sucesso!");
+
+		} catch (Exception e) {
+			System.out.println(e);
+		} finally {
+			//pst.close();
+		}
 	}
-	
-	public void alterar(Aluno a) {
+
+	public void alterar(int id, String nome, String sexo, LocalDate data) throws Exception{
+		try {
+			try {
+				con = db.getConexao();
+				sql = "UPDATE aluno  SET nome = ? , sexo = ?, dt_nasc = ? WHERE id = ?";
+				pst = con.prepareStatement(sql);
+				pst.setString(1, nome);
+				pst.setString(2, sexo);
+				Date dataSql = Date.valueOf(data);
+				pst.setDate(3, dataSql);
+				pst.setInt(4, id);
+				pst.executeUpdate();
+
+			} catch (Exception e) {
+				System.out.println(e);
+			} finally {
+				//pst.close();
+			}
+		} catch (Exception e) {
 		
+		}
 	}
 }
